2ba64abd910135b483b313c9b6543e04ad117297
Alejandro <alec.augal@gmail.com>
2017-08-10 15:51:56 +0200

    Update and rename bot.py to twitter_methods.py

--- twitter_methods.py
+++ twitter_methods.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# made with python 2.7
 """A twitter for console."""
 import tweepy
 
@@ -11,21 +10,22 @@ class MakingActions:
     def __init__(self, user, api):
         self.user = user
         self.api = api
-    
+
     def new_twit(self, twit):
         """Creates a new twit"""
         self.api.update_status(str(twit))
 
     def fav_twit(self, twit_id):
+        """Favorites the twit given"""
         self.api.favorites(twit_id)
-    
+
     def follow_all_followers(self, account_name):
         """Follow all the followers from an account"""
         ids = self.get_followers_id(account_name)
         #sigue a todos los users de una lista.
         for friends in ids:
             self.api.create_friendship(friends)
-    
+
     def get_followers_id(self, account_name):
         """Returns a list of all the followers of an account"""
         ids = []
@@ -33,9 +33,29 @@ class MakingActions:
             ids.extend(page)
         return ids
 
-    def get_following(self, account_name):
+    #def get_following(self, account_name):
+    #    """Return a list with all the following accounts"""
+    #   ids = []
+    #    for page in tweepy.Cursor(self.api.friends, screen_name=str(account_name)).pages():
+    #       ids.extend(page)
+    #  return ids
+    
+    def get_tweets_from_timeline(self):
+        """Returns a list of all the tweets from the home timeline"""
+        tweets = []
+        for tweets in tweepy.Cursor(self.api.home_timeline).items(200):
+            tweets.extend(tweets)
+        return tweets
+
+    def get_id_from_list(self, tweets_list):
+        """Gets the id from a list of twits and return a list of all id's from the tweets"""
         ids = []
-        for page in tweepy.Cursor(self.api.friends, screen_name=str(account_name)).pages():
-            ids.extend(page)
+        for tweets in tweets_list:
+            ids.append(tweets.id)
         return ids
 
+
+    def fav_from_list(self, list_id):
+        """Fav every twit in a list"""
+        for items in list_id:
+            self.fav_twit(items)
