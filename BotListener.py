fd932c0f05eef2b365b89f314fa4c8ff59e2791b
Alejandro <alec.augal@gmail.com>
2017-08-19 22:24:36 +0200

    Update BotListener.py

--- BotListener.py
+++ BotListener.py
@@ -4,6 +4,7 @@
 import tweepy
 from bot import MakingActions
 
+
 class PyTweListener(tweepy.StreamListener):
     """Hey"""
 
@@ -13,20 +14,18 @@ class PyTweListener(tweepy.StreamListener):
 
     def on_status(self, status):
         """Print a status text"""
+        status.text = status.text.lower()
         self.actions.fav_tweet(status.id)
-        print status.user.screen_name, status.created_at, status.text
+        print "@" + status.user.screen_name, status.created_at, status.text
         if "ultra kek 0 name" in status.text:
             self.actions.retweet(status.id)
         elif "y naci ciego" in status.text:
-            self.actions.fav_tweet(status.id)
             self.actions.retweet(status.id)
         elif "pytwe_bot" in status.text:
-            self.actions.fav_tweet(status.id)
             self.actions.retweet(status.id)
         elif "pickle rick" in status.text:
-            self.actions.fav_tweet(status.id)
             self.actions.retweet(status.id)
-            
+
     def on_error(self, status_code):
         if status_code == 420:
             #returning False in on_data disconnects the stream
