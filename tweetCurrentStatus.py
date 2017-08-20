c65f08c10bc33580eaf71ec9b0746cf9671331b8
Alejandro <alec.augal@gmail.com>
2017-08-20 18:33:10 +0200

    Add files via upload

---
+++ tweetCurrentStatus.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import tweepy
+from bot import MakingActions
+
+def main():
+    """Hace cosas"""
+    pass
+
+
+
+if __name__ == "__main__":
+    main()
