8e2ddbaaa53d7cce85820bf597e75e191296c28d
Alejandro <alec.augal@gmail.com>
2017-08-23 16:09:09 +0200

    Update tweet_current_status.py

--- tweet_current_status.py
+++ tweet_current_status.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# pylint: disable=R0914
 """Just a main"""
 import subprocess
 import time
@@ -19,19 +20,26 @@ def main():
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth, wait_on_rate_limit=True)
     while True:
+        uptime_command = subprocess.check_output(["uptime"])
+        tokenizer = nltk.tokenize.RegexpTokenizer(r'[0-9:]+')
+        tokenized_uptime = tokenizer.tokenize(uptime_command)
+        actual_uptime = tokenized_uptime[1]
         current_temp = subprocess.check_output(["/opt/vc/bin/vcgencmd", "measure_temp"])
         current_mem = subprocess.check_output(["free", "-h"])
         current_mem = current_mem.splitlines()
         tokenizer = nltk.tokenize.RegexpTokenizer(r'[M0-9]+')
-        tokenized_text = tokenizer.tokenize(current_mem[1])
+        tokenized_mem = tokenizer.tokenize(current_mem[1])
         cont = 0
-        for items in tokenized_text:
+        for items in tokenized_mem:
             if cont == 2:
                 used_mem = items
             elif cont == 3:
                 free_mem = items
             cont += 1
-        api.update_status("Current RPI 3 status: \n" + "Used Memory: " + str(used_mem) + "\nFree Memory: " + str(free_mem) + "\n" + str(current_temp) + "\n" + time.strftime("%H:%M:%S"))
+        api.update_status("Current RPI 3 status: \n" + "Used Memory: " + str(used_mem)
+                          + "\nFree Memory: " + str(free_mem) + "\n" + str(current_temp) + "\n"
+                          + "Uptime" + str(actual_uptime) + "\n"
+                          + "\n" + time.strftime("%H:%M:%S"))
         time.sleep(3600)
 if __name__ == "__main__":
     main()
