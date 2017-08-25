5e07414cdfd0355a0a4a861d9cc206c948a97e35
Alejandro <alec.augal@gmail.com>
2017-08-25 15:04:16 +0200

    Create main_quijote.py

---
+++ tweeting_quinote/main_quijote.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# pylint: disable=C1001
+"""docstring"""
+from time import sleep
+import tweepy
+from tweet_quijote import QuijoteTweet
+
+def main():
+    """Module"""
+    consumer_key = ""
+    consumer_secret = ""
+
+    access_token = ""
+    access_token_secret = ""
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
+
+    quijote = QuijoteTweet("quijote.txt", "kek.txt")
+
+    while True:
+        tweet = quijote.open_files()
+        api.update_status(tweet)
+        sleep(1800)
+
+if __name__ == "__main__":
+    main()
