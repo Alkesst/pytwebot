c8097b38f25536f571cf685cb798546d30d4da4d
Alejandro <alec.augal@gmail.com>
2017-08-26 22:19:13 +0200

    Update bot_listener.py

--- bot_listener.py
+++ bot_listener.py
@@ -23,12 +23,16 @@ from twitter_methods import MakingActions
         elif "pytwe_bot" in status.text:
             if status.user.id != self.actions.get_api().me().id and not status.entities['user_mentions']:
                 self.actions.quote_tweet("Dime", status)
+            print "@" + status.user.screen_name, status.created_at, status.text
         elif "putos catalufos" in status.text:
             self.actions.quote_tweet("deja de dar tanto asco porfa \n", status)
+            print "@" + status.user.screen_name, status.created_at, status.text
         elif status.text == 'nos vemo':
             self.actions.get_api().update_status("@" + status.user.screen_name + " en los bares", status.id)
+            print "@" + status.user.screen_name, status.created_at, status.text
         elif status.text == 'whenpp te pasa':
             self.actions.get_api().update_status('@' + status.user.screen_name + " si xD", status.id)
+            print "@" + status.user.screen_name, status.created_at, status.text
 
     def on_error(self, status_code):
         if status_code == 420:
